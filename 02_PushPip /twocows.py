cbb8dbbfd3cdf91681baeaaaf1b08efd76ade95b
ProximaFox <zlunick@mail.ru>
2025-02-23 20:43:44 +0300

    add arguments parsing
    positional arguments: message1, message2
    optional arguments: -e, -E, -f, -F, -n, -N, -h

--- 02_PushPip /twocows.py
+++ 02_PushPip /twocows.py
@@ -1,4 +1,6 @@
+import argparse
 from cowsay import cowsay
+
 
 def cow_concat(cows):
     cows_widths = []
@@ -12,6 +14,7 @@ def cow_concat(cows):
         cow_maxwidth = 0
         cow_lines_count = 0
         lines = cow.split('\n')
+
         for line in lines:
             cow_lines_count += 1
             cow_widths.append(len(line))
@@ -29,11 +32,55 @@ def cow_concat(cows):
     for i in range(cows_maxlines_count, 0, -1):
         for j in range(len(cows_lines_count)):
             if cows_lines_count[j] >= i:
-                result += cows_lines[j][0] + (cows_maxwidths[j] - cows_widths[j][0] + 1) * " "
+                result += cows_lines[j][0] + (cows_maxwidths[j] - cows_widths[j][0] + 1) * ' '
                 cows_lines[j].pop(0)
                 cows_widths[j].pop(0)
             else:
-                result += (cows_maxwidths[j] + 1) * " "
+                result += (cows_maxwidths[j] + 1) * ' '
         result += '\n'
     
     return result
+
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-e", default='oo', help=
+"""
+An eye string. This is ignored if a preset mode is given
+""")
+
+parser.add_argument("-E", default='oo', help=
+"""
+Same as -e, but for the second cow
+""")
+
+parser.add_argument("-f", default='default', help=
+"""
+Name of a cow specified in the COWPATH
+""")
+
+parser.add_argument("-F", default='default', help=
+"""
+Same as -f, but for the second cow
+"""
+)
+
+parser.add_argument("-n", action="store_true", help=
+"""
+If given, text in the speech bubble will not be wrapped
+"""
+)
+
+parser.add_argument("-N", action="store_true", help=
+"""
+Same as -n, but for the second cow
+"""
+)
+
+parser.add_argument("message1", type=str, help="first message")
+parser.add_argument("message2", type=str, help="first message")
+
+args = parser.parse_args()
+
+print(cow_concat([cowsay(args.message1, cow=args.f, eyes=args.e, wrap_text=(not args.n)),
+                  cowsay(args.message2, cow=args.F, eyes=args.E, wrap_text=(not args.N))]))
