e1f11e6c484ec3f1497b21317c6a0c6a89db2f4b
ProximaFox <zlunick@mail.ru>
2025-02-28 13:27:44 +0300

    add inform function

--- 03_MergeRequirements/bullcows.py
+++ 03_MergeRequirements/bullcows.py
@@ -7,7 +7,7 @@ def ask(prompt: str, valid: list[str] = None) -> str:
         
 
 def inform(format_string: str, bulls: int, cows: int) -> None:
-    pass
+    print(format_string.format(bulls, cows))
 
 def gameplay(ask: callable, inform: callable, words: list[str]) -> int:
     pass
